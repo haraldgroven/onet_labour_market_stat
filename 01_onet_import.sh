a4872397ac03caf617d8df41a2b1ed297a50052a
haraldgroven <haraldgroven@gmail.com>
2020-02-28 09:37:22 +0100

    Update 01_onet_import.sh

--- 01_onet_import.sh
+++ 01_onet_import.sh
@@ -54,19 +54,19 @@ mysql -Donet < ../03_translation_storage.sql && echo "finished creating tables f
 # import translations 
 
 mysql -Donet -e "TRUNCATE content_model_reference_nb"
-mysql --local-infile=ON -Donet -e "LOAD DATA LOCAL INFILE '../translations/content_model_reference_nb.tsv' REPLACE INTO TABLE content_model_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE '/tmp/translations/content_model_reference_nb.tsv' REPLACE INTO TABLE content_model_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
 echo "import of content_model_reference_nb"
 
 mysql -Donet -e "TRUNCATE dwa_reference_nb"
-mysql --local-infile=ON -Donet -e "LOAD DATA LOCAL INFILE '../translations/dwa_reference_nb.tsv' REPLACE INTO TABLE dwa_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE '/tmp/translations/dwa_reference_nb.tsv' REPLACE INTO TABLE dwa_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
 echo "import of dwa_reference_nb"
 
 mysql -Donet -e "TRUNCATE iwa_reference_nb"
-mysql --local-infile=ON -Donet -e "LOAD DATA LOCAL INFILE '../translations/iwa_reference_nb.tsv' REPLACE INTO TABLE iwa_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE '/tmp/translations/iwa_reference_nb.tsv' REPLACE INTO TABLE iwa_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
 echo "import of iwa_reference_nb"
 
 mysql -Donet -e "TRUNCATE occupation_data_nb"
-mysql --local-infile=ON -Donet -e "LOAD DATA LOCAL INFILE '../translations/occupation_data_nb.tsv' REPLACE INTO TABLE occupation_data_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE '/tmp/translations/occupation_data_nb.tsv' REPLACE INTO TABLE occupation_data_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
 echo "import of occupation_data_nb"
 
 # set new wd 
