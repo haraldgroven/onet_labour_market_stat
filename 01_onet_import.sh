ac73b0bb431745f6f48f424dc45a6a975f92c996
haraldgroven <haraldgroven@gmail.com>
2020-03-17 12:07:58 +0100

    24.2 db updates

--- 01_onet_import.sh
+++ 01_onet_import.sh
@@ -1,8 +1,8 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Import script occupational survey data from O*Net
 # source: US Bureau of Labor Statistics
-# Download from 
+# Download from
 # https://www.onetcenter.org/database.html#all-files
 # store data in MySQL database "onet"
 # version 0.1 - 2015-01
@@ -14,13 +14,13 @@ echo "Download from https://www.onetcenter.org/database.html#all-files"
 
 # Settings. These must be set before import
 
-# location of zipped database files of current onet release 
-onet_url="https://www.onetcenter.org/dl_files/database/db_24_1_mysql.zip"
+# location of zipped database files of current onet release
+onet_url="https://www.onetcenter.org/dl_files/database/db_24_2_mysql.zip"
 # my working directory. Also location of these Git-files
 # change or comment out if you use somewhere else!
 cd /srv/onet_labour_market_stat
 
-echo "importing from " $onet_url 
+echo "importing from " $onet_url
 echo "into " pwd
 echo "downloading data to dir onet_database_download"
 
@@ -28,51 +28,51 @@ echo "downloading data to dir onet_database_download"
 rm -rf onet_database_download
 mkdir -p onet_database_download
 cd onet_database_download && echo "wd onet_database_download"
-# download mysql data, extract without directory structure 
+# download mysql data, extract without directory structure
 curl -O $onet_url && tar xzf *mysql.zip --strip 1 && echo "curl download zip with database"
 rm *.zip
 
 # empty/re-create database
-mysql -e "DROP DATABASE IF EXISTS onet" && echo "Database onet removed " 
-mysql -e "CREATE DATABASE IF NOT EXISTS onet" && echo "Database onet (re-)created " 
+mysql -e "DROP DATABASE IF EXISTS onet" && echo "Database onet removed "
+mysql -e "CREATE DATABASE IF NOT EXISTS onet" && echo "Database onet (re-)created "
 
-# only import sql-files starting with two digits and underscore. 
-# merge these into one file, onet_db_all.sql 
+# only import sql-files starting with two digits and underscore.
+# merge these into one file, onet_db_all.sql
 cat [0-9][0-9]_*.sql > onet_db_all.sql && echo "All sql-files merged into file onet_db_all "
 
 echo "import data from onet_db_all.sql"
 mysql -Donet < onet_db_all.sql && echo "finished importing downloaded data from O*Net "
 
-# download tables for storing Norwegian translations of content 
+# download tables for storing Norwegian translations of content
 # curl -O "https://raw.githubusercontent.com/haraldgroven/onet_labour_market_stat/master/03_translation_storage.sql"
 
 mysql -Donet < ../03_translation_storage.sql && echo "finished creating tables from 03_translation_storage.sql "
 
 # clean up mess
-# rm onet_db_all.sql 
+# rm onet_db_all.sql
 
-# import translations 
+# import translations
 
 mysql -Donet -e "TRUNCATE content_model_reference_nb"
-mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE '/tmp/translations/content_model_reference_nb.tsv' REPLACE INTO TABLE content_model_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE 'translations/content_model_reference_nb.tsv' REPLACE INTO TABLE content_model_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
 echo "import of content_model_reference_nb"
 
 mysql -Donet -e "TRUNCATE dwa_reference_nb"
-mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE '/tmp/translations/dwa_reference_nb.tsv' REPLACE INTO TABLE dwa_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE 'translations/dwa_reference_nb.tsv' REPLACE INTO TABLE dwa_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
 echo "import of dwa_reference_nb"
 
 mysql -Donet -e "TRUNCATE iwa_reference_nb"
-mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE '/tmp/translations/iwa_reference_nb.tsv' REPLACE INTO TABLE iwa_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE 'translations/iwa_reference_nb.tsv' REPLACE INTO TABLE iwa_reference_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
 echo "import of iwa_reference_nb"
 
 mysql -Donet -e "TRUNCATE occupation_data_nb"
-mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE '/tmp/translations/occupation_data_nb.tsv' REPLACE INTO TABLE occupation_data_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql --local-infile=1 -Donet -e "LOAD DATA LOCAL INFILE 'translations/occupation_data_nb.tsv' REPLACE INTO TABLE occupation_data_nb FIELDS TERMINATED BY '\t' ENCLOSED BY '\"' IGNORE 1 LINES"
 echo "import of occupation_data_nb"
 
-# set new wd 
+# set new wd
 cd ..
 
-# compare translated strings to new import 
+# compare translated strings to new import
 mysql -Donet < 04_translation_maintenance.sql
 echo "number of entities which may need translation "
 
